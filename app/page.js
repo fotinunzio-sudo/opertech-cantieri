736ebe9eee0ce2386c46d7b875a0c6259334bf8c
fotinunzio-sudo <fotinunzio@gmail.com>
2026-04-07 22:31:15 +0200

    Update and rename page.js to page.tsx

--- app/page.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{ padding: 40, fontFamily: "Arial, sans-serif" }}>
-      <h1>Opertech Gestione Cantieri</h1>
-      <p>App online 🚀</p>
-    </div>
-  );
-}
---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: 40 }}>
+      <h1>🚀 Opertech - Gestione Cantieri</h1>
+      <p>La tua app è online</p>
+    </div>
+  );
+}
