273fb9b38701b50ce3d96039fab690d928835d42
fotinunzio-sudo <fotinunzio@gmail.com>
2026-04-07 20:43:01 +0200

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+export default function Home() {
+  return (
+    <div style={{ padding: 40, fontFamily: "Arial" }}>
+      <h1>Opertech - Gestione Cantieri</h1>
+      <p>Applicazione attiva su Render 🚀</p>
+
+      <div style={{ marginTop: 20 }}>
+        <p>✔ Deploy funzionante</p>
+        <p>✔ Database collegato</p>
+        <p>✔ Pronto per sviluppo</p>
+      </div>
+    </div>
+  );
+}
